f72dd7e05d5af73e33a5ba2e8473584d050862c5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-04 18:18:41 +0200

    honami: fix bt name
    Change-Id: I99d8fd16c20b6b27b709d76f0aacebf395b859b4

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Xperia Z Ultra"
+#define BTM_DEF_LOCAL_NAME   "Xperia Z1"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
