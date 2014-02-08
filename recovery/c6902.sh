face61c5d09b4d0edb13d8eca9876fa24a33830a
Chirayu Desai <cdesai@cyanogenmod.org>
2014-02-08 11:58:21 +0530

    Support C6902 with a post-install script
    * Modem firmware differs on C6902
    
    Change-Id: I3569e37bdd01511b7122620486a954487db5412f

---
+++ recovery/c6902.sh
@@ -0,0 +1,11 @@
+#!/sbin/sh
+
+mkdir -p /lta-label
+mount /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
+ls lta-label/*.html | grep -q c6903
+if [ $? -eq 0 ]; then
+	mount /dev/block/platform/msm_sdcc.1/by-name/system /system
+	cp /system/etc/firmware/c6902/* /system/etc/firmware/
+	umount /system
+fi
+umount /lta-label
