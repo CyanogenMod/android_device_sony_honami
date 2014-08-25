414d1e31cd9ff37b4d7a96609e7c7f0fd9777e25
OmarEinea <eineao@gmail.com>
2014-08-25 10:08:54 +0400

    honami: update signature (.133)
    Signed-off-by: Omar Einea <eineao@gmail.com>
    Change-Id: I342016e983900d6be6c1a7f2ef99c2a6f030aec4

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.4.4/14.4.A.0.133/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.4.4 14.4.A.0.133 ev__jQ release-keys"
 
 PRODUCT_NAME := cm_honami
 PRODUCT_DEVICE := honami
