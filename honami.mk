726e8a6b8c85bf70941000ae7f4b89df480a9a02
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-05-12 10:21:37 +0200

    Remove density from PRODUCT_AAPT_CONFIG
    Change-Id: I41bec62d180400356ff765ac1e680cc55128ee65

--- honami.mk
+++ honami.mk
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Device specific init
