13599071f2f96ce55f994e7ee69771c93d68262f
David Viteri <davidteri91@gmail.com>
2015-05-24 14:19:49 +0200

    honami: update fingerprint to latest lollipop release
    Change-Id: I23a4012368294226e373e526dd0ea9ca5c2482bd
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm.mk
+++ cm.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.4.4/14.4.A.0.133/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.4.4 14.4.A.0.133 ev__jQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.0.2/14.5.A.0.270/3750474323:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.0.2 14.5.A.0.270 3750474323 release-keys"
 
 PRODUCT_NAME := cm_honami
 PRODUCT_DEVICE := honami
