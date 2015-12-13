577520532d19b4c1be65a9e4ffb23242a262fe18
Chirayu Desai <chirayudesai1@gmail.com>
2015-12-13 09:15:37 +0530

    Update defconfig name, remove 'row'
    Change-Id: I1b2886f758632241e871b4fdb2c6d0ae4c6f9c8f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := cm_rhine_honami_row_defconfig
+TARGET_KERNEL_CONFIG := cm_rhine_honami_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
